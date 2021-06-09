89e78002c91ab27dcc41d48ae3b006864457d9f8
mahesh <mail.mahesh.lk@gmail.com>
2021-06-09 10:56:43 +0800

    spark-kafka

--- src/main/scala/SparkKafkaStream.scala
+++ src/main/scala/SparkKafkaStream.scala
@@ -1,5 +1,26 @@
 package io.mahesh
 
-object SparkKafkaStream {
 
+object SparkKafkaStream {
+  def main(args: Array[String]): Unit = {
+    print("hello")
+
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "localhost:9092,anotherhost:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "use_a_separate_group_id_for_each_stream",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
+
+    val topics = Array("topicA", "topicB")
+    val stream = KafkaUtils.createDirectStream[String, String](
+      streamingContext,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
+
+    stream.map(record => (record.key, record.value))
+  }
 }
