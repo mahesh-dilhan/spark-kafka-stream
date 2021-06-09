6b9a3dfd8700214d077ff5e0896bf3138051505d
mahesh <mail.mahesh.lk@gmail.com>
2021-06-09 13:34:09 +0800

    kafka-spark-streaming

--- build.sbt
+++ build.sbt
@@ -2,9 +2,16 @@ name := "spark-kafka-stream"
 
 version := "0.1"
 
-scalaVersion := "2.13.6"
+scalaVersion := "2.12.13"
 
 idePackagePrefix := Some("io.mahesh")
+
+lazy val commonSettings = Seq(
+  organization := "io.mahesh",
+  version := "0.1.0",
+  // set the Scala version used for the project.  2.11 isn't supported with Spark yet
+  scalaVersion := "2.12.13"
+)
 
 val spark = "org.apache.spark" % "spark-core_2.12" % "3.1.2"
 val sparkStreaming = "org.apache.spark" % "spark-streaming_2.12" % "3.1.2"
@@ -12,6 +19,10 @@ val sparkStreamKafka = "org.apache.spark" % "spark-streaming-kafka-0-10_2.12" % 
 
 
 lazy val root = (project in file(".")).
+  settings(commonSettings: _*).
+  settings(
+    name := "spark-kafka-stream"
+  ).
   settings(
     libraryDependencies += spark,
     libraryDependencies += sparkStreaming,
--- src/main/scala/SparkKafkaStream.scala
+++ src/main/scala/SparkKafkaStream.scala
@@ -1,6 +1,5 @@
 package io.mahesh
 
-
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
@@ -9,30 +8,30 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object SparkKafkaStream {
   def main(args: Array[String]): Unit = {
-    //print("hello")
 
     val conf = new SparkConf().setAppName("SparkKafkaStreaming").setMaster("local[*]")
-    val ssc = new StreamingContext(conf,Seconds(15))
+    val ssc = new StreamingContext(conf, Seconds(15))
     val sc = ssc.sparkContext
     sc.setLogLevel("ERROR")
 
-    val kafkaParams = Map[String, Object](
-      "bootstrap.servers" -> "localhost:9092",
+    val kafkaConf = Map(
+      "bootstrap.servers" -> "localhost:9092", // Default kafka broker list location
+      "zookeeper.connect" -> "localhost:2181", // Default zookeeper location
+      "group.id" -> "kafka-spark-streaming-example",
       "key.deserializer" -> classOf[StringDeserializer],
       "value.deserializer" -> classOf[StringDeserializer],
-      "group.id" -> "use_a_separate_group_id_for_each_stream",
-      "auto.offset.reset" -> "latest",
-      "enable.auto.commit" -> (false: java.lang.Boolean)
-    )
+      "zookeeper.connection.timeout.ms" -> "1000")
 
-    val topics = Array("country")
     val stream = KafkaUtils.createDirectStream[String, String](
       ssc,
       LocationStrategies.PreferConsistent,
-      ConsumerStrategies.Subscribe[String, String](topics, kafkaParams)
-    )
+      ConsumerStrategies.Subscribe[String, String](Set("country"), kafkaConf))
 
-   val lines = stream.map(record => (record.key, record.value))
+    val lines = stream.map(record => (record.key, record.value))
     lines.print()
+
+    ssc.start()
+    ssc.awaitTermination()
   }
+
 }
