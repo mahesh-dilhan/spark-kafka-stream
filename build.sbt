7f62ee62c7f3aeb540013fc4cb9d545b946e4989
mahesh <mail.mahesh.lk@gmail.com>
2021-06-09 12:45:26 +0800

    scala-spark-kafka

--- build.sbt
+++ build.sbt
@@ -5,3 +5,15 @@ version := "0.1"
 scalaVersion := "2.13.6"
 
 idePackagePrefix := Some("io.mahesh")
+
+val spark = "org.apache.spark" % "spark-core_2.12" % "3.1.2"
+val sparkStreaming = "org.apache.spark" % "spark-streaming_2.12" % "3.1.2"
+val sparkStreamKafka = "org.apache.spark" % "spark-streaming-kafka-0-10_2.12" % "3.1.2"
+
+
+lazy val root = (project in file(".")).
+  settings(
+    libraryDependencies += spark,
+    libraryDependencies += sparkStreaming,
+    libraryDependencies += sparkStreamKafka
+  )
--- src/main/scala/SparkKafkaStream.scala
+++ src/main/scala/SparkKafkaStream.scala
@@ -1,12 +1,23 @@
 package io.mahesh
 
 
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+
 object SparkKafkaStream {
   def main(args: Array[String]): Unit = {
-    print("hello")
+    //print("hello")
+
+    val conf = new SparkConf().setAppName("SparkKafkaStreaming").setMaster("local[*]")
+    val ssc = new StreamingContext(conf,Seconds(15))
+    val sc = ssc.sparkContext
+    sc.setLogLevel("ERROR")
 
     val kafkaParams = Map[String, Object](
-      "bootstrap.servers" -> "localhost:9092,anotherhost:9092",
+      "bootstrap.servers" -> "localhost:9092",
       "key.deserializer" -> classOf[StringDeserializer],
       "value.deserializer" -> classOf[StringDeserializer],
       "group.id" -> "use_a_separate_group_id_for_each_stream",
@@ -14,13 +25,14 @@ object SparkKafkaStream {
       "enable.auto.commit" -> (false: java.lang.Boolean)
     )
 
-    val topics = Array("topicA", "topicB")
+    val topics = Array("country")
     val stream = KafkaUtils.createDirectStream[String, String](
-      streamingContext,
-      PreferConsistent,
-      Subscribe[String, String](topics, kafkaParams)
+      ssc,
+      LocationStrategies.PreferConsistent,
+      ConsumerStrategies.Subscribe[String, String](topics, kafkaParams)
     )
 
-    stream.map(record => (record.key, record.value))
+   val lines = stream.map(record => (record.key, record.value))
+    lines.print()
   }
 }
